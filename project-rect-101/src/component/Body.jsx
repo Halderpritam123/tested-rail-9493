a7f6b74bbc88833c629f90bff8be3f2dd31f81b5
Halderpritam123 <halderpritamhalder1@gmail.com>
2023-02-23 23:40:19 +0530

    day3

--- project-rect-101/src/component/Body.jsx
+++ project-rect-101/src/component/Body.jsx
@@ -1,6 +1,8 @@
 import CaptionCarousel from './CaptionCarousel';
-import { Box, Heading, Img, Center } from '@chakra-ui/react';
+import { Box,Text, Heading, Img, Center } from '@chakra-ui/react';
 import Favourits from './FavouritesSlider';
+import Essential from './EssentialCarasole';
+import Footer from './Footer';
 function Body() {
   return (
     <Box>
@@ -20,16 +22,16 @@ function Body() {
       </Box>
       <Heading pl={90} py={5} fontSize='2xl'>First Purchase App Offers</Heading>
       <Center>
-        <Box p={6} display="grid" gridTemplateColumns='repeat(3, 1fr)' gap={10}>
-          <Box boxShadow='base' borderRadius="10px"><Img borderRadius="10px" src="https://images-static.nykaa.com/uploads/1476dbd5-7861-4e31-9fe9-aa27a6d60e14.jpg?tr=w-400,cm-pad_resize" />
+        <Box display="grid" gridTemplateColumns='repeat(3, 1fr)' gap={10}>
+          <Box  boxShadow='base' border="1px solid pink" borderRadius="10px"><Img borderRadius="10px" src="https://images-static.nykaa.com/uploads/1476dbd5-7861-4e31-9fe9-aa27a6d60e14.jpg?tr=w-400,cm-pad_resize" />
             <Heading color="#E80071" p={3} fontSize='xl'>Up To 30% Off</Heading>
           </Box>
-          <Box><Img borderRadius="10px" src="https://images-static.nykaa.com/uploads/8440b977-a365-4a3e-a5f9-b4c8df2e936c.jpg?tr=w-400,cm-pad_resize" /><Heading color="#E80071" p={3} fontSize='xl'>Up To 30% Off</Heading></Box>
-          <Box borderRadius="10px"><Img borderRadius="10px" src="https://images-static.nykaa.com/uploads/f2cf761b-b8a7-4168-b2d4-deebd99d6001.jpg?tr=w-400,cm-pad_resize" /><Heading color="#E80071" p={3} fontSize='xl'>Up To 30% Off</Heading>
+          <Box boxShadow='base' border="1px solid pink" borderRadius="10px"><Img borderRadius="10px" src="https://images-static.nykaa.com/uploads/8440b977-a365-4a3e-a5f9-b4c8df2e936c.jpg?tr=w-400,cm-pad_resize" /><Heading color="#E80071" p={3} fontSize='xl'>Up To 30% Off</Heading></Box>
+          <Box boxShadow='base' border="1px solid pink" borderRadius="10px"><Img borderRadius="10px" src="https://images-static.nykaa.com/uploads/f2cf761b-b8a7-4168-b2d4-deebd99d6001.jpg?tr=w-400,cm-pad_resize" /><Heading color="#E80071" p={3} fontSize='xl'>Up To 30% Off</Heading>
           </Box>
-          <Box borderRadius="10px"><Img borderRadius="10px" src="https://images-static.nykaa.com/uploads/1476dbd5-7861-4e31-9fe9-aa27a6d60e14.jpg?tr=w-400,cm-pad_resize" /><Heading color="#E80071" p={3} fontSize='xl'>Purifying properties of Bergamot</Heading></Box>
-          <Box><Img borderRadius="10px" src="https://images-static.nykaa.com/uploads/91c524dc-b0b8-467b-883e-b1d5fbed1e23.gif?tr=w-400,cm-pad_resize" /><Heading color="#E80071" p={3} fontSize='xl'>Up To 30% Off</Heading></Box>
-          <Box borderRadius="10px"><Img borderRadius="10px" src="https://images-static.nykaa.com/uploads/f2cf761b-b8a7-4168-b2d4-deebd99d6001.jpg?tr=w-400,cm-pad_resize" /><Heading color="#E80071" p={3} fontSize='xl'>Flat 10% off on ₹1299+</Heading></Box>
+          <Box boxShadow='base' border="1px solid pink" borderRadius="10px"><Img borderRadius="10px" src="https://images-static.nykaa.com/uploads/1476dbd5-7861-4e31-9fe9-aa27a6d60e14.jpg?tr=w-400,cm-pad_resize" /><Heading color="#E80071" p={3} fontSize='xl'>Purifying properties of Bergamot</Heading></Box>
+          <Box boxShadow='base' border="1px solid pink" borderRadius="10px"><Img borderRadius="10px" src="https://images-static.nykaa.com/uploads/91c524dc-b0b8-467b-883e-b1d5fbed1e23.gif?tr=w-400,cm-pad_resize" /><Heading color="#E80071" p={3} fontSize='xl'>Up To 30% Off</Heading></Box>
+          <Box boxShadow='base' border="1px solid pink" borderRadius="10px"><Img borderRadius="10px" src="https://images-static.nykaa.com/uploads/f2cf761b-b8a7-4168-b2d4-deebd99d6001.jpg?tr=w-400,cm-pad_resize" /><Heading color="#E80071" p={3} fontSize='xl'>Flat 10% off on ₹1299+</Heading></Box>
         </Box>
       </Center>
       <Center>
@@ -40,7 +42,7 @@ function Body() {
       </Center>
       <Box bg="#FFF6F9" mx={90}>
         <Box>
-          <Heading pl={90} py={5} fontSize='2xl'>Only At Nykaa</Heading>
+          <Heading pl={10} py={5} fontSize='2xl'>Only At Nykaa</Heading>
           <Center>
             <Box p={6} display="grid" gridTemplateColumns='repeat(3, 1fr)' gap={10}>
               <Box border={"1px solid pink"} boxShadow='base' borderRadius="10px"><Img borderRadius="10px" src="https://images-static.nykaa.com/creatives/fd337b94-586a-457c-8d66-335bb8d47db2/default.png?tr=w-400,cm-pad_resize" />
@@ -70,8 +72,85 @@ function Body() {
         <Heading pl={90} py={5} fontSize='2xl'>Discover Our Favourites</Heading>
         <Img px={90} w="100%" borderRadius="10px" src="https://images-static.nykaa.com/uploads/82eed1ab-64b2-4cb9-818b-9b39cd9284ec.jpg?tr=w-1200,cm-pad_resize" />
       </Box>
-      <Favourits/>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, distinctio, omnis praesentium officiis provident recusandae expedita laudantium adipisci corporis tempora pariatur nulla corrupti quae nobis? Laborum, ipsa qui dicta voluptate quas delectus incidunt atque velit repudiandae voluptas inventore blanditiis! Ratione est animi ipsam consectetur voluptas, laudantium quis nesciunt nihil commodi earum ullam et aspernatur, saepe provident minus quidem sequi ad dolore quae facilis temporibus aut. Debitis earum recusandae sed expedita commodi, eligendi, animi consequuntur repellat explicabo aliquam maiores dicta veritatis dolor quasi illum! Incidunt id ipsa, placeat obcaecati magnam ratione debitis ea voluptatem doloribus officiis aliquam labore architecto cumque inventore?
+      <Favourits />
+      <Box>
+        <Heading pl={90} py={5} fontSize='2xl'>Popular Picks</Heading>
+        <Center>
+          <Img px={90} w="100%" borderRadius="10px" src="https://images-static.nykaa.com/uploads/fd34eddd-690e-4f5b-b656-859c1e8df184.png?tr=w-1200,cm-pad_resize" />
+        </Center>
+        <Box></Box>
+        {/* append High rating data */}
+      </Box>
+      <Box>
+        <Heading pl={90} py={5} fontSize='2xl'>Featured Brands
+        </Heading>
+        <Center>
+          <Box p={12} display="grid" gridTemplateColumns='repeat(5, 1fr)' gap={10}>
+            <Box border={"1px solid pink"} boxShadow='base' borderRadius="10px"><Img w="100%" borderRadius="10px" src="https://images-static.nykaa.com/creatives/7665aa19-e8cf-4b7e-81fe-948377539e14/default.jpg?tr=w-240,cm-pad_resize" />
+              <Heading p={3} fontSize='lg'>Up To 20% Off</Heading>
+            </Box>
+            <Box border={"1px solid pink"} boxShadow='base' borderRadius="10px"><Img w="100%" borderRadius="10px" src="https://images-static.nykaa.com/creatives/22e90ee9-35c1-4a42-b25f-fc62fb45d613/default.png?tr=w-240,cm-pad_resize" /><Heading p={3} fontSize='lg'>Up To 30% Off</Heading></Box>
+            <Box border={"1px solid pink"} boxShadow='base' borderRadius="10px"><Img w="100%" borderRadius="10px" src="https://images-static.nykaa.com/creatives/3d014b89-d850-41f6-8999-2ca0a7758214/default.jpg?tr=w-240,cm-pad_resize" /><Heading p={3} fontSize='lg'>91% Saw Brighter Skin in Weeks</Heading>
+            </Box>
+            <Box border={"1px solid pink"} boxShadow='base' borderRadius="10px"><Img w="100%" borderRadius="10px" src="https://images-static.nykaa.com/creatives/3d014b89-d850-41f6-8999-2ca0a7758214/default.jpg?tr=w-240,cm-pad_resize" /><Heading p={3} fontSize='lg'>91% Saw Brighter Skin in Weeks</Heading>
+            </Box>
+            <Box border={"1px solid pink"} boxShadow='base' borderRadius="10px"><Img w="100%" borderRadius="10px" src="https://images-static.nykaa.com/creatives/3d014b89-d850-41f6-8999-2ca0a7758214/default.jpg?tr=w-240,cm-pad_resize" /><Heading p={3} fontSize='lg'>91% Saw Brighter Skin in Weeks</Heading>
+            </Box>
+          </Box>
+        </Center>
+        <Center>
+          <Box px={12} display="grid" gridTemplateColumns='repeat(4, 1fr)' gap={10}>
+            <Box border={"1px solid pink"} boxShadow='base' borderRadius="10px"><Img w="100%" borderRadius="10px" src="https://images-static.nykaa.com/creatives/49395ef6-0458-49fe-911f-2a415c602046/default.png?tr=w-240,cm-pad_resize" />
+              <Heading p={3} fontSize='lg'>Up To 20% Off</Heading>
+            </Box>
+            <Box border={"1px solid pink"} boxShadow='base' borderRadius="10px"><Img w="100%" borderRadius="10px" src="https://images-static.nykaa.com/creatives/de9fd13c-72b0-4728-846c-fcd70df8e602/default.jpg?tr=w-240,cm-pad_resize" /><Heading p={3} fontSize='lg'>Up To 30% Off</Heading></Box>
+            <Box border={"1px solid pink"} boxShadow='base' borderRadius="10px"><Img w="100%" borderRadius="10px" src="https://images-static.nykaa.com/creatives/626dee77-f0de-4ea1-94f0-9b5bde33f977/default.jpg?tr=w-240,cm-pad_resize" /><Heading p={3} fontSize='lg'>91% Saw Brighter Skin in Weeks</Heading>
+            </Box>
+            <Box border={"1px solid pink"} boxShadow='base' borderRadius="10px"><Img w="100%" borderRadius="10px" src="https://images-static.nykaa.com/creatives/fb25aee3-8b4a-4f36-abc0-845590245aac/default.jpg?tr=w-240,cm-pad_resize" /><Heading p={3} fontSize='lg'>91% Saw Brighter Skin in Weeks</Heading>
+            </Box>
+          </Box>
+        </Center>
+        <Center>
+          <Box px={12} py={6} display="grid" gridTemplateColumns='repeat(2, 1fr)' gap={10}>
+            <Box border={"1px solid pink"} boxShadow='base' borderRadius="10px"><Img w="100%" borderRadius="10px" src="https://images-static.nykaa.com/creatives/49395ef6-0458-49fe-911f-2a415c602046/default.png?tr=w-240,cm-pad_resize" />
+              <Heading p={3} fontSize='lg'>Up To 20% Off</Heading>
+            </Box>
+            <Box border={"1px solid pink"} boxShadow='base' borderRadius="10px"><Img w="100%" borderRadius="10px" src="https://images-static.nykaa.com/creatives/de9fd13c-72b0-4728-846c-fcd70df8e602/default.jpg?tr=w-240,cm-pad_resize" /><Heading p={3} fontSize='lg'>Up To 30% Off</Heading></Box>
+          </Box>
+        </Center>
+      </Box>
+      <Box bg="rgb(229, 242, 244)" pb={10}>
+        <Box>
+          <Heading pl={90} py={5} fontSize='2xl'>Everyday Essentials</Heading>
+          <Img pb={10} w="100%" px={10} src="https://images-static.nykaa.com/uploads/2adda7a5-567a-4db1-b408-06c96be60287.jpg?tr=w-1200,cm-pad_resize"/>
+         <Essential/>
+        </Box>
+      </Box>
+      <Box>
+        <Heading pt={6} fontSize='2xl' pl={10}>Game Zone</Heading>
+        <Text pl={10}>Play & Win</Text>
+        <Box borderRadius="10px" mt={5} mx={10} bgImage="url('https://images-static.nykaa.com/uploads/22eaef70-8ee8-45ec-9d15-2dab3ea9f621.jpg?tr=w-1200,cm-pad_resize')" bgSize="100%">
+        <Box py={120} pl={10} w="50%">
+        <Heading fontSize='6xl' color="white">Exciting Offers</Heading>
+        <Heading fontSize='3xl' color="white">On Your Favourite Brands</Heading>
+        </Box>
+        </Box>
+        <Box>
+          <Heading pt={6} pl={10} fontSize='2xl'>Beauty Advice</Heading>
+          <Text pl={10}>Discover Latest Tips & Tricks</Text>
+          <Box p={10} display="grid" gridTemplateColumns="repeat(2,1fr)" gap={10}>
+          <Box border={"1px solid pink"} boxShadow='base' borderRadius="10px"><Img w="100%" borderRadius="10px" src="https://images-static.nykaa.com/uploads/71c7558d-637c-40ee-b09b-7dfda35cf125.jpg?tr=w-600,cm-pad_resize" /><Heading p={3} fontSize='lg'>Catch Up On Top Trends, Expert Advice, Fresh Launches & More</Heading>
+          <Heading pl={5} fontSize='md' color="#E80071">Watch Now</Heading></Box>
+          <Box border={"1px solid pink"} boxShadow='base' borderRadius="10px"><Img w="100%" borderRadius="10px" src="https://images-static.nykaa.com/uploads/e7c366df-a513-4932-9cdc-fbeda4a707a5.jpg?tr=w-600,cm-pad_resize" /><Heading p={3} fontSize='lg'>Catch Up On Top Trends,Expert Advice, Fresh Launches & More</Heading>
+          <Heading pl={5} fontSize='md' color="#E80071">Read More</Heading>
+          </Box>
+          </Box>
+        </Box>
+      </Box>
+      <Img px={10} w="100%" borderRadius="10px" src="https://images-static.nykaa.com/uploads/49e43a50-37b5-405a-be2d-ab90055115ec.jpg?tr=w-1200,cm-pad_resize"/>
+      <Box borderRadius="10px" mt={7} mx={10} color="white"  bg="#8c8d94">
+      <Footer/>
+      </Box>
     </Box>
   )
 
--- project-rect-101/src/component/CarouselProduct.jsx
+++ project-rect-101/src/component/CarouselProduct.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import {Box,Img,Text} from '@chakra-ui/react';
 export default function CarouselProduct(props) {
   return (
-    <Box w="98%" h={250} className="card" bgImage={props.url} >
-      <Box pl={2} pt="65%" >
+    <Box w="800px" h={250} className="card" bgImage={props.url}  >
+      <Box pl={2} pt="62%" >
         <Text color="white">{props.head1}</Text>
         <Text color="white">{props.head2}</Text>
       </Box>
