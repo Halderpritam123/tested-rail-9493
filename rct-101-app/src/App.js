3ba53257a40e8d80b8cecddddb827429fe526f92
Halderpritam123 <halderpritamhalder1@gmail.com>
2023-03-07 16:14:04 +0530

    2

--- rct-101-app/src/App.js
+++ rct-101-app/src/App.js
@@ -1,24 +1,14 @@
 import logo from './logo.svg';
+import {Box} from '@chakra-ui/react';
 import './App.css';
-
+import NavBar from './component/NavBar';
+import AllRoutes from './component/AllRoutes';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box className="App">
+  <NavBar/>
+    <AllRoutes/>
+    </Box>
   );
 }
 
---
+++ rct-101-app/src/component/AdsCarosole.jsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import { Box,Img, IconButton, useBreakpointValue } from '@chakra-ui/react';
+// Here we have used react-icons package for the icons
+import { BiLeftArrowAlt, BiRightArrowAlt } from 'react-icons/bi';
+// And react-slick as our Carousel Lib
+import Slider from 'react-slick';
+
+// Settings for the slider
+const settings = {
+  dots: true,
+  arrows: false,
+  fade: true,
+  infinite: true,
+  autoplay: true,
+  speed: 500,
+  autoplaySpeed: 5000,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+};
+
+export default function AdsCarousel() {
+  // As we have used custom buttons, we need a reference variable to
+  // change the state
+  const [slider, setSlider] = React.useState(null);
+
+  // These are the breakpoints which changes the position of the
+  // buttons as the screen size changes
+  const top = useBreakpointValue({ base: '90%', md: '50%' });
+  const side = useBreakpointValue({ base: '30%', md: '10px' });
+
+  // These are the images used in the slide
+  const cards = [
+    'https://images-static.nykaa.com/creatives/6908c7a1-0dc5-447d-8396-200bd11b3a51/default.jpg?tr=w-1200,cm-pad_resize',
+    'https://images-static.nykaa.com/creatives/ca6834b2-873d-472a-a1bd-e95c2063d624/default.jpg?tr=w-1200,cm-pad_resize',
+    'https://images-static.nykaa.com/creatives/4b56d4f1-9a52-4965-9c8d-a8972fc70c20/default.jpg?tr=w-1200,cm-pad_resize',
+  ];
+
+  return (
+    <Box
+    border="1px solid black"
+      position={'relative'}
+      height={'380px'}
+      width={'full'}
+      overflow={'hidden'}>
+      {/* CSS files for react-slick */}
+      <link
+        rel="stylesheet"
+        type="text/css"
+        charSet="UTF-8"
+        href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+      />
+      <link
+        rel="stylesheet"
+        type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+      />
+      {/* Left Icon */}
+      <IconButton
+        aria-label="left-arrow"
+        colorScheme="messenger"
+        borderRadius="full"
+        position="absolute"
+        left={side}
+        top={top}
+        transform={'translate(0%, -50%)'}
+        zIndex={2}
+        onClick={() => slider?.slickPrev()}>
+        <BiLeftArrowAlt />
+      </IconButton>
+      {/* Right Icon */}
+      <IconButton
+        aria-label="right-arrow"
+        colorScheme="messenger"
+        borderRadius="full"
+        position="absolute"
+        right={side}
+        top={top}
+        transform={'translate(0%, -50%)'}
+        zIndex={2}
+        onClick={() => slider?.slickNext()}>
+        <BiRightArrowAlt />
+      </IconButton>
+      {/* Slider */}
+      <Slider {...settings} ref={(slider) => setSlider(slider)}>
+        {cards.map((url) => (
+            <Img src={url} />
+        ))}
+      </Slider>
+    </Box>
+  );
+}
