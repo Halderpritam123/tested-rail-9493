63a643370c56c090a12f5c7ef1f23888be390d2f
Halderpritam123 <halderpritamhalder1@gmail.com>
2023-03-07 19:21:24 +0530

    5

---
+++ rct-101-app/src/component/NavBar.jsx
@@ -0,0 +1,392 @@
+import React from 'react';
+import { AiOutlineMobile } from "react-icons/ai";
+import { CiLocationOn } from "react-icons/ci";
+import { MdCardGiftcard } from "react-icons/md";
+import { FiHelpCircle } from "react-icons/fi";
+import Cart from "./Cart";
+import logo from "./logo.png"
+import offer from "./offer.jpg"
+import Navoptions from "./Navoption";
+import { Navigate } from "react-router-dom";
+import {
+  Box,
+  Flex,
+  Text,
+  IconButton,
+  Button,
+  Stack,
+  Link,
+  Collapse,
+  PopoverHeader,
+  PopoverBody,
+  PopoverFooter,
+  PopoverArrow,
+  PopoverCloseButton,
+  PopoverAnchor,
+  Icon,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  useColorModeValue,
+  useBreakpointValue,
+  useDisclosure,
+  Input,
+  Img,
+  Tooltip,
+  Center
+} from '@chakra-ui/react';
+import {
+  HamburgerIcon,
+  CloseIcon,
+  ChevronDownIcon,
+  ChevronRightIcon,
+} from '@chakra-ui/icons';
+
+export default function NavBar() {
+  const { isOpen, onToggle } = useDisclosure();
+
+  return (
+    <Box>
+      <Box pos="fixed" textAlign="center" fontFamily="sans-serif" w="100%" zIndex={99999}>
+        <Box display="flex" justifyContent="space-between" px="7%" py="8px" bgImage="url('https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/7a65e265-c116-4a4a-aeeb-7146aa802331/dblrd58-faf297c6-247b-48bb-a2ce-030f7c26a651.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzdhNjVlMjY1LWMxMTYtNGE0YS1hZWViLTcxNDZhYTgwMjMzMVwvZGJscmQ1OC1mYWYyOTdjNi0yNDdiLTQ4YmItYTJjZS0wMzBmN2MyNmE2NTEucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.U2QT58dVPsh8f9T2UDTdCSFBIuCKn6rTkVWDCDieHYA')" >
+          <Box>
+            BEAUTY BONANZA Get Your Daily Dose Of Amazing Deals</Box>
+          <Box >
+            <Link href='#' isExternal >
+              <Icon as={AiOutlineMobile} />Get App
+            </Link> |
+            <Link href='#' isExternal >
+              <Icon as={CiLocationOn} />Store & Events
+            </Link> |
+            <Link href='#' isExternal >
+              <Icon as={MdCardGiftcard} />Gift Card
+            </Link> |
+            <Link href='#' isExternal >
+              <Icon as={FiHelpCircle} />Help
+            </Link>
+          </Box>
+        </Box>
+        <Flex
+          bg={useColorModeValue('white', 'gray.800')}
+          color={useColorModeValue('gray.600', 'white')}
+          minH={'60px'}
+          // py={{ base: 2 }}
+          px={{ base: 50 }}
+          borderBottom={1}
+          borderStyle={'solid'}
+          borderColor={useColorModeValue('gray.200', 'gray.900')}
+          align={'center'} >
+          <Flex
+            flex={{ base: 1, md: 'auto' }}
+            ml={{ base: -2 }}
+            display={{ base: 'flex', md: 'none' }}>
+            <IconButton
+              onClick={onToggle}
+              icon={
+                isOpen ? <CloseIcon w={3} h={3} /> : <HamburgerIcon w={5} h={5} />
+              }
+              variant={'ghost'}
+              aria-label={'Toggle Navigation'}
+            />
+          </Flex>
+          <Flex flex={{ base: 1 }} justify={{ base: 'center', md: 'start' }}>
+            {/* -------------------------- */}
+           <Text
+              textAlign={useBreakpointValue({ base: 'center', md: 'left' })}
+              fontFamily={'heading'}
+              color={useColorModeValue('gray.800', 'white')}>
+              <Img onClick={ <Navigate to="/"/>} w={100} src={logo} align="center" p={1} />
+            </Text>
+
+            <Flex display={{ base: 'none', md: 'flex' }} ml={10}>
+              <DesktopNav />
+            </Flex>
+            <Box px="2%" w="40vw">
+
+              <Input placeholder="Search on Sykaa" />
+            </Box>
+
+          </Flex>
+
+           <Stack
+            flex={{ base: 1, md: 0 }}
+            justify={'flex-end'}
+            direction={'row'}
+            spacing={6}>
+                <Button
+                  as={'a'}
+                  fontSize={'sm'}
+                  fontWeight={600}
+                  color={'white'}
+                  bg={'pink.400'}
+                  href={'#'}
+                  _hover={{
+                    bg: 'pink.300',
+                  }}>
+                 <Link href="/login"> Sign In</Link>
+                </Button>
+
+          </Stack>
+          {/* --------------------------------- */}
+          <Cart />
+        </Flex>
+        <Collapse in={isOpen} animateOpacity>
+          <MobileNav />
+        </Collapse>
+        <Box borderBottom={1}
+          borderStyle={'solid'}
+          borderColor={useColorModeValue('gray.200', 'gray.900')} py={2} display="flex" justifyContent="space-around" px={50} align={Center} bg="white" boxShadow="rgba(0, 0, 0, 0.24) 0px 3px 8px">
+          <Link href='/makeup'><Tooltip maxWidth="500%" bg="white" color="black" label={<Navoptions />}  >
+            Makeup
+          </Tooltip></Link>
+          <Tooltip maxWidth="500%"
+            bg="white" color="black"
+            label={<Navoptions />} >
+            Skin
+          </Tooltip>
+          <Tooltip maxWidth="500%" bg="white" color="black" label={<Navoptions />} aria-label='A tooltip'>
+            Hair
+          </Tooltip>
+          <Tooltip maxWidth="500%" bg="white" color="black" label={<Navoptions />} aria-label='A tooltip'>
+            Appliances
+          </Tooltip>
+          <Tooltip maxWidth="500%" bg="white" color="black" label={<Navoptions />} aria-label='A tooltip'>
+            Bath & Body
+          </Tooltip>
+          <Tooltip maxWidth="500%" bg="white" color="black" label={<Navoptions />} aria-label='A tooltip'>
+            Natural
+          </Tooltip>
+          <Tooltip maxWidth="500%" bg="white" color="black" label={<Navoptions />} aria-label='A tooltip'>
+            Mom & Baby
+          </Tooltip>
+          <Tooltip maxWidth="500%" bg="white" color="black" label={<Navoptions />} aria-label='A tooltip'>
+            Health & Wellness
+          </Tooltip>
+          <Tooltip maxWidth="500%" bg="white" color="black" label={<Navoptions />} aria-label='A tooltip'>
+            Men
+          </Tooltip>
+          <Tooltip maxWidth="500%" bg="white" color="black" label={<Navoptions />} aria-label='A tooltip'>
+            Fragrance
+          </Tooltip>
+          <Tooltip maxWidth="500%" bg="white" color="black" label={<Navoptions />} aria-label='A tooltip'>
+            Pop Ups
+          </Tooltip>
+          <Box w="4%">
+            <Img w="100%" src={offer} />
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
+
+const DesktopNav = () => {
+  const linkColor = useColorModeValue('gray.600', 'gray.200');
+  const linkHoverColor = useColorModeValue('gray.800', 'white');
+  const popoverContentBgColor = useColorModeValue('white', 'gray.800');
+
+  return (
+    <Stack direction={'row'} spacing={4}>
+      {NAV_ITEMS.map((navItem) => (
+        <Box key={navItem.label}>
+          <Popover trigger={'hover'} placement={'bottom-start'}>
+            <PopoverTrigger>
+              <Link
+                p={2}
+                href={navItem.href ?? '#'}
+                fontSize={'sm'}
+                fontWeight={500}
+                color={linkColor}
+                _hover={{
+                  textDecoration: 'none',
+                  color: linkHoverColor,
+                }}>
+                {navItem.label}
+              </Link>
+            </PopoverTrigger>
+
+            {navItem.children && (
+              <PopoverContent
+                border={0}
+                boxShadow={'xl'}
+                bg={popoverContentBgColor}
+                p={4}
+                rounded={'xl'}
+                minW={'sm'}>
+                <Stack>
+                  {navItem.children.map((child) => (
+                    <DesktopSubNav key={child.label} {...child} />
+                  ))}
+                </Stack>
+              </PopoverContent>
+            )}
+          </Popover>
+        </Box>
+      ))}
+    </Stack>
+  );
+};
+
+const DesktopSubNav = ({ label, href, subLabel }: NavItem) => {
+  return (
+    <Link
+      href={href}
+      role={'group'}
+      display={'block'}
+      p={2}
+      rounded={'md'}
+      _hover={{ bg: useColorModeValue('pink.50', 'gray.900') }}>
+      <Stack direction={'row'} align={'center'}>
+        <Box>
+          <Text
+            transition={'all .3s ease'}
+            _groupHover={{ color: 'pink.400' }}
+            fontWeight={500}>
+            {label}
+          </Text>
+          <Text fontSize={'sm'}>{subLabel}</Text>
+        </Box>
+        <Flex
+          transition={'all .3s ease'}
+          transform={'translateX(-10px)'}
+          opacity={0}
+          _groupHover={{ opacity: '100%', transform: 'translateX(0)' }}
+          justify={'flex-end'}
+          align={'center'}
+          flex={1}>
+          <Icon color={'pink.400'} w={5} h={5} as={ChevronRightIcon} />
+        </Flex>
+      </Stack>
+    </Link>
+  );
+};
+
+const MobileNav = () => {
+  return (
+    <Stack
+      bg={useColorModeValue('white', 'gray.800')}
+      p={4}
+      display={{ md: 'none' }}>
+      {NAV_ITEMS.map((navItem) => (
+        <MobileNavItem key={navItem.label} {...navItem} />
+      ))}
+    </Stack>
+  );
+};
+
+const MobileNavItem = ({ label, children, href }: NavItem) => {
+  const { isOpen, onToggle } = useDisclosure();
+
+  return (
+    <Stack spacing={4} onClick={children && onToggle}>
+      <Flex
+        py={2}
+        as={Link}
+        href={href ?? '#'}
+        justify={'space-between'}
+        align={'center'}
+        _hover={{
+          textDecoration: 'none',
+        }}>
+        <Text
+          fontWeight={600}
+          color={useColorModeValue('gray.600', 'gray.200')}>
+          {label}
+        </Text>
+        {children && (
+          <Icon
+            as={ChevronDownIcon}
+            transition={'all .25s ease-in-out'}
+            transform={isOpen ? 'rotate(180deg)' : ''}
+            w={6}
+            h={6}
+          />
+        )}
+      </Flex>
+
+      <Collapse in={isOpen} animateOpacity style={{ marginTop: '0!important' }}>
+        <Stack
+          mt={2}
+          pl={4}
+          borderLeft={1}
+          borderStyle={'solid'}
+          borderColor={useColorModeValue('gray.200', 'gray.700')}
+          align={'start'}>
+          {children &&
+            children.map((child) => (
+              <Link key={child.label} py={2} href={child.href}>
+                {child.label}
+              </Link>
+            ))}
+        </Stack>
+      </Collapse>
+    </Stack>
+  );
+};
+
+interface NavItem {
+  label: string;
+  subLabel?: string;
+  children?: Array<NavItem>;
+  href?: string;
+}
+
+const NAV_ITEMS: Array<NavItem> = [
+  {label: 'Home',
+    href:'/'},
+  {
+
+    label: 'Categories',
+    children: [
+      {
+        label: <Link to="makeup">Makeup</Link>,
+        // subLabel: 'Face',
+        href: '/makeup',
+      },
+      {
+        label: 'Skin',
+        // subLabel: 'Up-and-coming Designers',
+        // href: '#',
+      },
+      {
+        label: 'Hair',
+        // subLabel: 'Up-and-coming Designers',
+        // href: '#',
+      },
+      {
+        label: 'Appliances',
+        // subLabel: 'Up-and-coming Designers',
+        // href: '#',
+      },
+      {
+        label: 'Bath & Body',
+        // subLabel: 'Up-and-coming Designers',
+        // href: '#',
+      },
+      {
+        label: 'Natural',
+        // subLabel: 'Up-and-coming Designers',
+        // href: '#',
+      },
+    ],
+  },
+  {
+    label: 'Brands',
+    children: [
+      {
+        // label: Navoptions(),
+      },
+    ],
+  },
+  {
+    label: 'Luxe',
+  },
+  {
+    label: 'Nykaa Fashion',
+    href: '#',
+  },
+  {
+    label: 'Beauty Advice',
+    href: '#',
+  },
+];
---
+++ rct-101-app/src/component/caurasoldata.js
@@ -0,0 +1,100 @@
+export const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 1024 },
+      items: 4,
+      slidesToSlide: 2,
+    },
+    desktop: {
+      breakpoint: { max: 1024, min: 800 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 800, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+  
+  export const productData = [
+    {
+      id: 1,
+      imageurl:
+      "https://images-static.nykaa.com/uploads/8e38f956-76f6-4a96-a250-b69573e66696.jpg?tr=w-600,cm-pad_resize",
+      head1:"Up To 50% Off",
+      head2:"Buy 4 & get additional 10% Off"
+    },
+    {
+      id: 2,
+      imageurl:
+      "https://images-static.nykaa.com/uploads/79c1face-6380-476d-9cef-22bfbc4023cb.gif?tr=w-600,cm-pad_resize",
+      head1:"Budget-Friendly Cleansers",
+      head2:"Up To 35% Off"
+    },
+    {
+      id: 3,
+      imageurl:
+      "https://images-static.nykaa.com/uploads/33c4212d-241b-41de-a8d2-c7b8d7d9cc13.jpg?tr=w-600,cm-pad_resize",
+      head1:"Up To 50% Off",
+      head2:"Buy 4 & get additional 10% Off"
+    },
+    {
+      id: 4,
+      imageurl:
+      "https://images-static.nykaa.com/uploads/8523b59b-1695-4a5d-bbc5-2f918bac1b05.jpg?tr=w-600,cm-pad_resize",
+      head1:"Budget-Friendly Cleansers",
+      head2:"Up To 35% Off"
+    },
+    {
+      id: 5,
+      imageurl:
+      "https://images-static.nykaa.com/uploads/25a842ec-b66a-46f3-8c67-dd4e5178d5be.jpg?tr=w-600,cm-pad_resize",
+      head1:"Budget-Friendly Cleansers",
+      head2:"Up To 35% Off"
+    },
+    {
+      id: 6,
+      imageurl:
+      "https://images-static.nykaa.com/uploads/d2052bfb-fc68-46ae-a16e-eb0645f19c89.png?tr=w-600,cm-pad_resize",
+      head1:"Budget-Friendly Cleansers",
+      head2:"Up To 35% Off"
+    },
+    {
+      id: 7,
+      imageurl:
+      "https://images-static.nykaa.com/uploads/9fa1e381-68e0-40ba-b1d3-7d63928dec0c.jpg?tr=w-600,cm-pad_resize",
+      head1:"Budget-Friendly Cleansers",
+      head2:"Up To 35% Off"
+    },
+    {
+      id: 8,
+      imageurl:
+      "https://images-static.nykaa.com/uploads/8523b59b-1695-4a5d-bbc5-2f918bac1b05.jpg?tr=w-600,cm-pad_resize",
+      head1:"Budget-Friendly Cleansers",
+      head2:"Up To 35% Off"
+    },
+    {
+      id: 9,
+      imageurl:
+      "https://images-static.nykaa.com/uploads/25a842ec-b66a-46f3-8c67-dd4e5178d5be.jpg?tr=w-600,cm-pad_resize",
+      head1:"Budget-Friendly Cleansers",
+      head2:"Up To 35% Off"
+    },
+    {
+      id: 10,
+      imageurl:
+      "https://images-static.nykaa.com/uploads/d2052bfb-fc68-46ae-a16e-eb0645f19c89.png?tr=w-600,cm-pad_resize",
+      head1:"Budget-Friendly Cleansers",
+      head2:"Up To 35% Off"
+    },
+    {
+      id: 11,
+      imageurl:
+      "https://images-static.nykaa.com/uploads/9fa1e381-68e0-40ba-b1d3-7d63928dec0c.jpg?tr=w-600,cm-pad_resize",
+      head1:"Budget-Friendly Cleansers",
+      head2:"Up To 35% Off"
+    },
+  ];
